c37542d04f478d82c2bf61dce5358279393a1a9c
nshutilancelot3 <n.lancelot@alustudent.com>
2026-04-04 01:35:14 +0200

    fixing

---
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Fix Apache 500 error by correcting the .phpp typo in WordPress files
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/',
+}
